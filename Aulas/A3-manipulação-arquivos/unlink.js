c1b6d3661c3a47cb0281d57f0179cba7171e27c5
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-10-23 19:34:21 -0300

    add

--- Aulas/A3-manipulação-arquivos/unlink.js
+++ Aulas/A3-manipulação-arquivos/unlink.js
@@ -1,8 +1,8 @@
 const fs = require('node:fs');
 
-fs.unlink('arqui.txt', (err) => {
+fs.unlink('arquiv.txt', (err) => {
     if (err) {
-        console.log('Erro ao deletar o arquivo:', err);
+        console.log('Erro ao deletar o arquivo:', err.message);
         return;
     }
     console.log('Arquivo deletado com sucesso!');
