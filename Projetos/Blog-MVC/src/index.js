1d99a568e1a70269937bb3b73d8fe22b49ac35df
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-11-04 19:59:08 -0300

    Projeto mvc iniciado

---
+++ Projetos/Blog-MVC/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const path = require('node:path');
+const router = require('./routes');
+
+const PORT = process.env.PORT || 3000;
+
+// Configuração do EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+// Configuração de arquivos estáticos
+app.use(express.static('../public'))
+
+// Configuração para ler dados da requisição
+app.use(express.urlencoded({ extended: true }));
+
+// Rotas da aplicação 
+app.use(router)
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: \nhttp://localhost:${PORT}`);
+})
