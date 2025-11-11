285548006dfc416be0f8bc6ce1ca466991028cd9
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-11-11 19:49:32 -0300

    Finalizando o app

--- Exercicios/web-node/ex-02/models/taskListModel.js
+++ Exercicios/web-node/ex-02/models/taskListModel.js
@@ -1,28 +1,55 @@
 let taskLists = [
-    {id: '1' , title: 'Estudos'},
-    {id: '2' , title: 'Tarefas de casa'},
-    {id: '3' , title: 'Tarefas do serviço'},
-    {id: '4' , title: 'Compras'},
-
-];
+    { id: '1', title: 'Estudos', tasks: [{ id: '1', title: 'Estudar Node', completed: false }] },
+    { id: '2', title: 'Tarefas de casa', tasks: [] },
+    { id: '3', title: 'Coisas do trabalho', tasks: [] }
+]
 
 module.exports = {
     getAllTaskLists: () => {
-        return taskLists;
+        return taskLists
+    },
+
+    getTaskListById: (id) => {
+        return taskLists.find(list => list.id === id)
     },
 
     createList: (title) => {
-        const newList =  {
+        const newList = {
             id: Math.floor(Math.random() * 99999999).toString(),
-            title: title
+            title: title,
+            tasks: []
         }
-
-        return newList;
+        return newList
     },
 
     saveList: (taskList) => {
-        if (taskList.title === '' )  throw new Error('titulo da lista está vazio');
-        
-        taskLists.push(taskList);
+        if (taskList.title === '') throw new Error('Título da lista não pode ficar vazio.')
+        taskLists.push(taskList)
+    },
+
+    deleteList: (listId) => {
+        const listIndex = taskLists.findIndex(list => list.id === listId)
+        taskLists.splice(listIndex, 1)
+    },
+
+    addTask: (listId, taskTitle) => {
+        const newTask = {
+            id: Math.floor(Math.random() * 99999999).toString(),
+            title: taskTitle,
+            completed: false
+        }
+        taskLists.find(list => list.id === listId).tasks.push(newTask)
+    },
+
+    completeTask: (listId, taskId) => {
+        const taskList = taskLists.find(list => list.id === listId)
+        const task = taskList.tasks.find(task => task.id === taskId)
+        task.completed = true
+    },
+
+    undoTask: (listId, taskId) => {
+        const taskList = taskLists.find(list => list.id === listId)
+        const task = taskList.tasks.find(task => task.id === taskId)
+        task.completed = false
     }
 }
--- Exercicios/web-node/ex-02/src/server.js
+++ Exercicios/web-node/ex-02/src/server.js
@@ -1,24 +1,14 @@
+const express = require('express')
+const path = require('path')
+const router = require('./routes.js')
+const app = express()
 
-const express = require('express');
-const path = require('path');
-const router = require('./router');
-const app = express();
-
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true}));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
 
-const PORT = process.env.PORT || 3000;
+app.use(router)
 
-app.use(router);
-
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
-
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}`))
